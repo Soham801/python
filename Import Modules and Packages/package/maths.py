4d4d22e16f5c481ddb916930c1a496611748d88c
Soham801 <sohamdeshmukh801@gmail.com>
2026-02-07 09:46:52 +0530

    Working on Import modules

--- Import Modules and Packages/package/maths.py
+++ Import Modules and Packages/package/maths.py
@@ -1,2 +1,7 @@
 def addition(a,b):
     return a+b
+def EvenOdd(x):
+    if(x % 2 == 0):
+        print(f"{x} is an Even Number ")
+    else:
+        print(f"{x} is a Odd Number ")
---
+++ Import Modules and Packages/test.py
@@ -0,0 +1,4 @@
+from package.maths import *
+
+print(EvenOdd(249))
+print(addition(48,57))
